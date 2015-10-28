94679f9648b26b7e2d8edbbaa7c1d85966545a2f
Melissa BR <melissa.benali-richard@etu.utc.fr>
2015-10-28 13:07:25 -0500

    Creation of the abstract class Observation

---
+++ src/entities/Observation.java
@@ -0,0 +1,28 @@
+package entities;
+
+abstract public class Observation {
+	
+	private int[][] features;
+	private int realLabel;
+	
+	public Observation(int[][] features, int realLabel){
+		this.features = features;
+		this.realLabel = realLabel;
+	}	
+	
+	public Observation(int[][] features){
+		this.features = features;
+	}
+	
+	public int getFeatures(int i, int j){
+		return features[i][j];
+	}
+	
+	public int getRealLabel(){
+		return realLabel;
+	}
+	
+	public void setRealLabel(int realLabel){
+		this.realLabel = realLabel;
+	}
+}
