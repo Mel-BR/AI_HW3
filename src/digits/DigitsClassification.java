67acf93b50200a7d1b9c260cd66a4b969d5ae7f1
Melissa BR <melissa.benali-richard@etu.utc.fr>
2015-10-29 18:49:11 -0500

    Update main method for the digit classification problem

--- src/digits/DigitsClassification.java
+++ src/digits/DigitsClassification.java
@@ -12,23 +12,20 @@ public class DigitsClassification {
 	public static void main(String arg[]){
 		
 		System.out.println("Reading training observations...");
-		ArrayList<TrainObservation> trainObsList = HelpFuncs.buildTrainObs();		
+		ArrayList<TrainObservation> trainObsList = HelpFuncs.buildTrainObs("trainingimages","traininglabels");		
 		
 		System.out.println("Reading test observations...");
-		ArrayList<TestObservation> testObsList = HelpFuncs.buildTestObs();
-		
+		ArrayList<TestObservation> testObsList = HelpFuncs.buildTestObs("testimages","testlabels");
+
 		System.out.println("Done");
 		
 		Classifier classifier = new Classifier(28, 10, 2, trainObsList.size(), 1);
 		classifier.train(trainObsList);
-		ArrayList<TestObservation> testObsListComplete = classifier.test(testObsList);
-		
-		System.out.println();
-		
-		
-		for(TestObservation testObs : testObsListComplete){
+		ArrayList<TestObservation> testObsListLabeled = classifier.test(testObsList);
+
+		/*for(TestObservation testObs : testObsListLabeled){
 			System.out.println(testObs.getRealLabel()+" "+testObs.getPredictedLabel());
-		}
+		}*/
 		
 		
 	}	
